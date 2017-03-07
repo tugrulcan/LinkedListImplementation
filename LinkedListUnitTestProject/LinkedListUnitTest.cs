5166ee78a41788a5fd3db2972c061e6dc57b217a
Tugrul Can <tugrulcansollu@gmail.com>
2017-03-08 02:58:21 +0300

    Added  - Test methods for InsertLast()

--- LinkedListUnitTestProject/LinkedListUnitTest.cs
+++ LinkedListUnitTestProject/LinkedListUnitTest.cs
@@ -23,7 +23,6 @@ namespace LinkedListUnitTestProject
             string actualResult = list.DisplayElements();
 
             Assert.AreEqual(expectedResult, actualResult);
-
         }
 
         [TestMethod]
@@ -51,9 +50,11 @@ namespace LinkedListUnitTestProject
             list.InsertFirst(0);
 
             Assert.AreEqual(list.Size, 3);
-            Assert.AreEqual(list.Head.Data, 0);
-            Assert.AreEqual(list.Head.Next, n1);
-            Assert.AreEqual(list.Head.Next.Next, n2);
+
+            string expectedResult = "0 1 2 ";
+            string actualResult = list.DisplayElements();
+            Assert.AreEqual(expectedResult, actualResult);
+
             Assert.IsNull(list.Head.Next.Next.Next);
         }
 
@@ -64,10 +65,51 @@ namespace LinkedListUnitTestProject
             list.InsertFirst(0);
 
             Assert.AreEqual(list.Size, 1);
-            Assert.AreEqual(list.Head.Data, 0);
+
+            string expectedResult = "0 ";
+            string actualResult = list.DisplayElements();
+            Assert.AreEqual(expectedResult, actualResult);
+
+            Assert.IsNull(list.Head.Next);
+        }
+
+        [TestMethod]
+        public void DoesInsertLastMethodAddElementToEndOfListWhenListIsNotEmpty()
+        {
+            Node n1 = new Node() { Data = 1 };
+            Node n2 = new Node() { Data = 2 };
+            n1.Next = n2;
+            n2.Next = null;
+
+            LinkedList list = new LinkedList();
+            list.Head = n1;
+            list.Size = 2;
+
+            list.InsertLast(3);
+            Assert.AreEqual(list.Size, 3);
+
+            string expectedResult = "1 2 3 ";
+            string actualResult = list.DisplayElements();
+            Assert.AreEqual(expectedResult, actualResult);
+
+            Assert.IsNull(list.Head.Next.Next.Next);
+
+        }
+
+        [TestMethod]
+        public void DoesInsertLastMethodAddElementToEndOfListWhenListIsEmpty()
+        {
+            LinkedList list = new LinkedList();
+
+            list.InsertLast(3);
+            Assert.AreEqual(list.Size, 1);
+
+            string expectedResult = "3 ";
+            string actualResult = list.DisplayElements();
+            Assert.AreEqual(expectedResult, actualResult);
+
             Assert.IsNull(list.Head.Next);
 
         }
-
-        }
+    }
 }
