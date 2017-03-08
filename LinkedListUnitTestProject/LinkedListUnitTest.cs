9ed92a850da2d14437456c1e34edc07e692af2f3
Tugrul Can <tugrulcansollu@gmail.com>
2017-03-08 06:38:20 +0300

    Added Test methods for DeletePos()

--- LinkedListUnitTestProject/LinkedListUnitTest.cs
+++ LinkedListUnitTestProject/LinkedListUnitTest.cs
@@ -326,5 +326,54 @@ namespace LinkedListUnitTestProject
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+        [TestMethod]
+        public void DoesDeletePosMethodDeleteTheDesiredElementinListWhenListHasOneElement()
+        {
+            Node n4 = new Node() { Data = 142, Next = null };
+            Node n3 = new Node() { Data = 122 , Next = n4};
+            Node n2 = new Node() { Data = 11 , Next= n3};
+            Node n1 = new Node() { Data = 34, Next =n2 };
+
+            LinkedList list = new LinkedList() { Head = n1, Size = 4 };
+
+            list.DeletePos(2);
+            Assert.AreEqual(3, list.Size);
+
+            string expectedResult = "34 11 142 ";
+            string actualResult = list.DisplayElements();
+            Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void DoesDeletePosMethodThrowErrorWhenListIsEmpty()
+        {
+            LinkedList list = new LinkedList();
+            list.DeletePos(0);
+            Assert.AreEqual(0, list.Size);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void DoesDeletePosMethodThrowErrorWhenPositionIsNegative()
+        {
+            Node n1 = new Node() { Data = 10 };
+            n1.Next = null;
+            LinkedList list = new LinkedList() { Head = n1, Size=1};
+            list.DeletePos(-2);
+            Assert.AreEqual(1, list.Size);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void DoesDeletePosMethodThrowErrorWhenPositionIsLargerThanSize()
+        {
+            Node n1 = new Node() { Data = 10 };
+            n1.Next = null;
+            LinkedList list = new LinkedList() { Head = n1, Size = 1 };
+            list.DeletePos(1);
+            Assert.AreEqual(0, list.Size);
+        }
+
+    }
 }
